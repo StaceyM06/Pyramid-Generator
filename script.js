6e06ca2b332e68d2390b0eace8fc8c62b742c272
Stacey Masse <162021785+StaceyM06@users.noreply.github.com>
2024-08-09 10:57:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+  rows.push(padRow(i + 1, count));
 }
 
 let result = "";
